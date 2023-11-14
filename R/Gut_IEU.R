812d9ed9c4a74fc8f4e359228246d1dfddf40d12
shaoming1995 <116997703+shaoming1995@users.noreply.github.com>
2023-11-14 15:02:30 +0800

    Add files via upload

--- R/Gut_IEU.R
+++ R/Gut_IEU.R
@@ -17,16 +17,15 @@ Gut_IEU<-function(savefile,PATH,GWASID,outname,kb,r2){
 for (i in filename[,1]){
   ipath<-paste0(PATH,"\\",i)
   exp_temp<-read.csv(ipath,header = T)
-  exp_temp<-clump_data(exp_temp,clump_kb = kb,clump_r2 = r2)
-  #获取结局数据的工具变量--Lung cancer
-  test2<-(try(OUT<-extract_outcome_data(snps=exp_temp$SNP,outcomes=GWASID,proxies=T,maf_threshold = 0.01,access_token = NULL)))
-  if(class(test2) == "try-error"){
-   OUT<-extract_outcome_data(snps=exp_temp$SNP,outcomes=GWASID,proxies=T,maf_threshold = 0.01,access_token = NULL)
+  test2 <- (try(exp_temp1 <- clump_data(exp_temp, clump_kb = kb,
+                                       clump_r2 = r2)))
+  if (class(test2) == "try-error") {
+   OUT<-extract_outcome_data(snps=exp_temp1$SNP,outcomes=GWASID,proxies=T,maf_threshold = 0.01,access_token = NULL)
    if(dim(OUT)[[1]]!=0){
    OUT$id.outcome<-outname
    OUT$outcome<-outname
    OUT<-OUT[!duplicated(OUT$SNP),]
-   exp_temp_out<-merge(exp_temp,OUT,by="SNP",all=F)
+   exp_temp_out<-merge(exp_temp1,OUT,by="SNP",all=F)
    exp_temp_out$eaf.exposure<-NA
    #分别取出暴露与结局的数据
    exp<- exp_temp_out[,c("SNP","effect_allele.exposure","other_allele.exposure","beta.exposure","se.exposure",
@@ -36,6 +35,12 @@ for (i in filename[,1]){
    #调控方向
    dat <-harmonise_data(exposure_dat=exp,outcome_dat=out,action=2)
    res <- mr(dat)
+   res$cluster <- 1    #0表达富集成功
+   mr_OR<-generate_odds_ratios(res)
+   mr_OR$or<-round(mr_OR$or,3)
+   mr_OR$or_lci95<-round(mr_OR$or_lci95,3)
+   mr_OR$or_uci95 <- round(mr_OR$or_uci95,3)
+   mr_OR$OR_CI <- paste0(mr_OR$or,"(",mr_OR$or_lci95,"-",mr_OR$or_uci95,")")
    het <- mr_heterogeneity(dat)
    ple <- mr_pleiotropy_test(dat)
    A_temp<-rbind(res,A_temp)
@@ -47,9 +52,11 @@ for (i in filename[,1]){
    Cname<-paste0(savefile,"\\","肠道菌群与",outname,"的多效性结果.csv")
    write.csv(A_temp,Aname,row.names = F)
    write.csv(B_temp,Bname,row.names = F)
-   write.csv(C_temp,Cname,row.names = F)}else{
-     cat(i,"与",outname,"未找到工具变量不进行计算")}
-   }else{
+   write.csv(C_temp,Cname,row.names = F)}
+   else{
+     cat(i,"与",outname,"未找到工具变量不进行计算")}}
+  else{
+  OUT<-extract_outcome_data(snps=exp_temp$SNP,outcomes=GWASID,proxies=T,maf_threshold = 0.01,access_token = NULL)
   if(dim(OUT)[[1]]!=0){
   OUT$id.outcome<-outname
   OUT$outcome<-outname
@@ -64,6 +71,12 @@ for (i in filename[,1]){
     #调控方向
   dat <-harmonise_data(exposure_dat=exp,outcome_dat=out,action=2)
       res <- mr(dat)
+      res$cluster <- 0    #0表达没有富集成功
+      mr_OR<-generate_odds_ratios(res)
+      mr_OR$or<-round(mr_OR$or,3)
+      mr_OR$or_lci95<-round(mr_OR$or_lci95,3)
+      mr_OR$or_uci95 <- round(mr_OR$or_uci95,3)
+      mr_OR$OR_CI <- paste0(mr_OR$or,"(",mr_OR$or_lci95,"-",mr_OR$or_uci95,")")
       het <- mr_heterogeneity(dat)
       ple <- mr_pleiotropy_test(dat)
       A_temp<-rbind(res,A_temp)
